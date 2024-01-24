5848100a89efec7a2b5b7bea3060412fb0b4ea97
Luciefifi <niyomutonilucie@gmail.com>
2024-01-24 21:50:16 +0200

    carriculum vitae

---
+++ src/App.jsx
@@ -0,0 +1,162 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='parent-container'>
+      <div className='part1'>
+      <div className="image">
+        <img src="" alt="photo" />
+      </div>
+      <div className="contacts">
+        <div className="email">
+          <p>icon</p>
+          <p><a href="">niymutonilucie@gmail.com</a></p>
+        </div>
+        <div className="linkedin">
+        <p>icon</p>
+          <p><a href="">niymutonilucie@gmail.com</a></p>
+        </div>
+        <div className="linkedin">
+        <p>icon</p>
+          <p><a href="">niymutonilucie@gmail.com</a></p>
+        </div>
+        <div className="linkedin">
+        <p>icon</p>
+          <p><a href="">niymutonilucie@gmail.com</a></p>
+        </div>
+        <div className="linkedin">
+        <p>icon</p>
+          <p><a href="">niymutonilucie@gmail.com</a></p>
+        </div>
+      </div>
+      <div className="education">
+        <ul>
+          <li>University</li>
+          <li>secondary</li>
+        </ul>
+      </div>
+      <div className="skills">
+        <ul>
+          <li>Leadership skills</li>
+          <li>Leadership skills</li>
+          <li>Leadership skills</li>
+          <li>Leadership skills</li>
+          <li>Leadership skills</li>
+          <li>Leadership skills</li>
+        </ul>
+      </div>
+      <div className="awards">
+        <ul>
+          <li>Mastercard faundation</li>
+          <li>Mastercard faundation</li>
+          <li>Mastercard faundation</li>
+          <li>Mastercard faundation</li>
+          <li>Mastercard faundation</li>
+          <li>Mastercard faundation</li>
+        </ul>
+      </div>
+      <div className="references">
+
+      </div>
+
+      </div>
+      <div className="part2">
+        <div className="names">
+          <h2>Lucie NIYOMUTONI</h2>
+          <p>Sofware engineer</p>
+        </div>
+        <div className="experience">
+          <h3>EXPERINECES</h3>
+          <div className="andela">
+          <h5>Andela</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+
+          </div>
+          <div className="cmu">
+            <h5>CMU-AFRICA</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+
+
+          </div>
+          <div className="openskies">
+            <h5>OPEN SKIES</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+
+          </div>
+          <div className="volunteer">
+            <h5>VOLUNTEER</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+
+          </div>
+          <div className="iee">
+            <h5>Teaching assistant</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+
+          </div>
+          <div className="mastercard">
+            <h5>MASTERCARD</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rem, ut quaerat commodi suscipitaccusamus dolor, 
+            id ea modi quas a alias voluptates minus voluptatem, impedit consectetur vitae nisi pariatur.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim laudantium facere recusandae consequatur voluptatem quasi iusto cum, ad impedit ducimus, suscipit dolorem magni sed praesentium quam dolorum officia debitis veniam!</p>
+
+          </div>
+   
+        </div>
+
+      </div>
+      <div className="expertise">
+        <p>React native</p>
+        <p>React native</p>
+        <p>React native</p>
+        <p>React native</p>
+        <p>React native</p>
+        <p>React native</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
